164179785809d47c4b48ce1bc5d8cd580121c49a
Dennis White <denniswhite98@yahoo.com>
2015-03-06 23:57:47 -0800

    logout
    adding logout view for tabor

---
+++ GeoFriends/GFLogoutViewController.h
@@ -0,0 +1,13 @@
+//
+//  GFLogoutViewController.h
+//  GeoFriends
+//
+//  Created by Dennis White on 3/6/15.
+//  Copyright (c) 2015 dniswhite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GFLogoutViewController : UIViewController
+
+@end
